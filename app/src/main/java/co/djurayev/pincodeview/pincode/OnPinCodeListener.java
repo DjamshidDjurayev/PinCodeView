d69723921e0645747e79fbec409d155c3bf08c01
Djamshid <djurayev.d@gmail.com>
2019-07-25 01:11:35 +0500

    added annotation step

--- app/src/main/java/co/djurayev/pincodeview/pincode/OnPinCodeListener.java
+++ app/src/main/java/co/djurayev/pincodeview/pincode/OnPinCodeListener.java
@@ -1,11 +1,12 @@
 package co.djurayev.pincodeview.pincode;
 
 import co.djurayev.pincodeview.pincode.annotations.PinModes;
+import co.djurayev.pincodeview.pincode.annotations.PinSteps;
 
 public interface OnPinCodeListener {
   void onPinInputCompleted(String pin, @PinModes int mode);
 
-  void onPinStepChange(String oldPin, @PinModes int mode, int step);
+  void onPinStepChange(String oldPin, @PinModes int mode, @PinSteps int step);
 
   void onPinError(String oldPin, String pin, @PinModes int mode);
 }
